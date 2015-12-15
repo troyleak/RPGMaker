42bc644776ef78d05248340ca06da89dfaf77301
troy <troy.leak@gmail.com>
2015-12-15 15:05:12 -0800

    CSS added
    Added a gitignore to handle secrets
    
    Added CSS and div tags
    
    Need to have SECRET_KEY environment variable set now for flask to come
    up

--- RPGMaker.py
+++ RPGMaker.py
@@ -2,22 +2,29 @@
 
 from flask import Flask, render_template
 
+import secrets
+
 from character import *
 from attributes import *
 from abilities import *
 from skills import *
 
 app = Flask(__name__)
+app.secret_key = secrets.SECRET_KEY
 
 @app.route("/")
 def pathfinder():
     player = character(app) # create new character object to pass around and build
 
-    attrib_form = Attributes(csrf_enabled=False)
-    ability_form = Abilities(csrf_enabled=False)
-    skills_form = Skills(csrf_enabled=False)
+    attrib_form = Attributes()
+    ability_form = Abilities()
+    skills_form = Skills()
 
     return render_template('pathfinder.html', attrib_form=attrib_form, ability_form=ability_form, skills_form=skills_form)
+
+@app.route("/submit/")
+def submit():
+    print("Hello")
 
 if __name__ == "__main__":
     app.run(debug=True)
