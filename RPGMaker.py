1cfefae63550aeba12ec20de7ba2bef8ccd31e02
troy <troy.leak@gmail.com>
2015-12-17 01:18:23 -0800

    Fixed some frontend caps issues and added some docstrings

--- RPGMaker.py
+++ RPGMaker.py
@@ -1,4 +1,38 @@
 #! /usr/local/bin/python3
+'''
+RPGMaker
+
+Creates a character sheet for Pathfinder RPG
+
+User can put in as much information as they want. Values not submitted will be
+randomized. After randomization, returns character for review.
+
+User can modify character sheet as many times as they need.
+
+At the end the character sheet can be printed to PDF (or other file)
+
+
+
+Main program:
+
+    Creates character and passes it around. The end result will be
+    a tree-like structure containing at the highest level only information about
+    basic attributes like class, race, etc. The attribute classes will contain
+    information about and application logic for the options of that attribute
+
+
+    I realize this is large in scope, most of the logic should already be
+    implemented though
+
+    TODO:   Need to tie frontend and backend together. Most of the hard work should
+                already be done. Just have to have form submission update character
+                model based on validation results
+
+            Error catching - can be done in the individual classes
+
+            Fix frontend glitches - (low priority)
+
+'''
 
 from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
@@ -27,9 +61,6 @@ def create_app(configfile=None):
 
         if (form.validate_on_submit()):
             return redirect(url_for(submit), created=True)
-            # submit will return a different view if checks pass
-        else:
-            flash("Error validating form, try again")
 
         return render_template('charsheet.html', form=form)
 
--- items.py
+++ items.py
@@ -1,45 +1,101 @@
-"""
-Adds the item to the character object and changes attributes based on the form
-"""
+'''
+Class Items
+
+Contains classes for each type of item the player is capable of carrying
+Right now the Gear class is not able to directly modify stats.
+That *may* change, but not right now.
+
+To use it, create an Items object and add child objects to it
+
+For example:
+
+
+    character.items = Items
+
+    weapon = Weapon.make("parameters", "go", "here")
+
+    character.items.add_weapon(weapon)
+
+
+TODO: Determine if input validation is necessary. If so, do that
+
+'''
 
 class Weapon():
-    # Adds a weapon object to a character
-    # takes a character object and a weapon object
-    def __init__():
-        weapon = {'name':"" ,
-                'attack_bonus':0 ,
-                'critical':0 ,
-                'dmg_type':0 ,
-                'wpn_range':0 ,
-                'ammunition':0 ,
-                'damage':"" }
 
-    def add_weapon(character, weapon):
-        character.weapons += weapon
+    def __init__(self, master):
+        self.weapon = { 'name':"" ,
+                        'attack_bonus':0 ,
+                        'critical':0 ,
+                        'dmg_type':0 ,
+                        'wpn_range':0 ,
+                        'ammunition':0 ,
+                        'damage':"" }
+        print("Created uninitialized Weapon item")
 
+    def make(self, name, attack_bonus, critical, dmg_type, wpn_range, ammunition, damage):
+        self.name = name
+        self.attack_bonus = attack_bonus
+        self.critical = critical
+        self.dmg_type = dmg_type
+        self.wpn_range = wpn_range
+        self.ammunition = ammunition
+        self.damage = damage
+        print("Initialized Weapon")
 
 
 class Armor():
-    # Adds an armor object to a character
-    def __init__():
-        armor = {'name':"" ,
-                'armor_bonus':0 ,
-                'type':"" ,
-                'check_penalty':0 ,
-                'spell_failure':0 ,
-                'weight':0 ,
-                'properties':"" }
 
-    def add_armor(character, armor):
-        character.armor += armor
+    def __init__(self, master):
+        self.armor =  { 'name':"" ,
+                        'armor_bonus':0 ,
+                        'armor_type':"" ,
+                        'check_penalty':0 ,
+                        'spell_failure':0 ,
+                        'weight':0 ,
+                        'properties':"" }
+        print("Created uninitialized Armor item")
 
+    def make(self, name, armor_bonus, armor_type, check_penalty, spell_failure, weight, properties):
+        self.name = name
+        self.armor_bonus = armor_bonus
+        self.type = armor_type
+        self.check_penalty = check_penalty
+        self.spell_failure = spell_failure
+        self.weight = weight
+        self.properties = properties
+        print("Initialized Armor")
 
 
 class Gear():
-    # adds a gear object to a character
-    def __init__():
-        gear = {'name':"" ,
-                'weight':0 }
 
-    def add_gear(character, gear):
-        character.gear += gear
+    def __init__(self, master):
+        self.gear = {'name':"" ,
+                     'weight':0,
+                     'properties':"" }
+        print("Created uninitialized Gear item")
+
+    def make(self, name, weight, properties):
+        self.name = name
+        self.weight = weight
+        self.properties = properties
+        print("Initialized Gear")
+
+
+class Items():
+
+    def __init__(self, master):
+        self.weapons = []
+        self.armor = []
+        self.gear = []
+
+    def add_weapon(self, weapon):
+        self.weapons += weapon
+
+
+    def add_armor(self, armor):
+        self.armor += armor
+
+
+    def add_gear(self, gear):
+        self.gear += gear
